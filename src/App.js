5bde5e3289316c212554f51103299f27ac0075da
mchis1908 <mchis19.08@gmail.com>
2023-03-17 23:36:48 +0700

    updated2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Chi</h1>
+        <h1>Chi dep trai hon Kien</h1>
       </header>
     </div>
   );
